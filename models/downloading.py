6d4617b54f851741fc63e4be94ebcae5b242394d
XuF163 <15380362192@163.com>
2024-05-11 22:59:19 +0800

    没用的东西

--- models/downloading.py
+++ models/downloading.py
@@ -6,7 +6,7 @@ import yaml
 # 创建 Client 对象，连接到 qBittorrent 的 Web API
 qb = Client("http://192.168.1.75:8080/")
 #读取账号密码配置
-with open('config.yaml', 'r') as f:
+with open('../config/config.yaml', 'r') as f:
     config = yaml.safe_load(f)
 # 登录 qBittorrent
 qb.login(config['username'], config['passwd'])
