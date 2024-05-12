3c6a510cfaa198e70c0259f95ea00546b3ea7050
XuF163 <15380362192@163.com>
2024-05-12 16:38:29 +0800

    配置文件读取

--- index.js
+++ index.js
@@ -9,7 +9,16 @@ const dirname = path.dirname(filePath)
 const basename = path.basename(dirname)
 /** 插件包相对路径 */
 const dirPath = './plugins/' + basename
+//配置文件
+//检查config目录下是否为空
+const configPath = path.join(dirname, 'config', 'config.yaml')
+const defConfigPath = path.join(dirname, 'config', 'defSet', 'config.yaml')
+const configFileExists = common.fileExists(configPath)
+// 如果config目录下的config.yaml文件不存在，则从defSet目录下的config.yaml复制到config目录下
+if (!configFileExists) {
+  common.copyFile(defConfigPath, configPath)
+}
 
 export { dirPath }
 
-logger.info(basename + ' 插件 0.0.1初始化~')
+logger.info(basename + ' 插件 0.0.1 初始化~')
